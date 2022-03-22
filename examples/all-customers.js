8ea0eba4626e3598cf79a65fe7122ba1756de7b4
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-03-22 13:26:00 -0600

    fix: lint

--- examples/all-customers.js
+++ examples/all-customers.js
@@ -1,5 +1,5 @@
-const tuneuptechnology = require('tuneuptechnology')
+const tuneuptechnology = require('tuneuptechnology');
 
-const client = new tuneuptechnology.client(process.env.API_EMAIL, process.env.API_KEY)
+const client = new tuneuptechnology.client(process.env.API_EMAIL, process.env.API_KEY);
 
-client.customers.all().then(console.log).catch(console.log)
+client.customers.all().then(console.log).catch(console.log);
