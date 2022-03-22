78323b0993d5c192ab5c5c9c4461d2361d7cf6e1
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-03-22 13:21:04 -0600

    fix: example file names

---
+++ examples/create-customer.js
@@ -0,0 +1,15 @@
+const tuneuptechnology = require('../src/tuneuptechnology')
+
+const client = new tuneuptechnology.client(process.env.API_EMAIL, process.env.API_KEY)
+
+const data = {
+  firstname: 'Jake',
+  lastname: 'Peralta',
+  email: 'jake@example.com',
+  phone: '8015551234',
+  user_id: 1,
+  notes: 'Believes he is a good detective.',
+  location_id: 1
+}
+
+client.customers.create(data).then(console.log).catch(console.log)
